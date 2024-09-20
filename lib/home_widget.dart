e1f42fbbaebf2e80e429902418d1365af4146b65
gabrielcoelhosev <gabrielcoelhoseverino6915@gmail.com>
2024-09-20 20:39:33 -0300

    att tela inicial

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -18,8 +18,7 @@ class _HomeWidgetState extends State<HomeWidget> {
   final List _photos = [
     Data(image: "assets/imgs/coxinha.jpg", text: "Coxinha Low Carb"),
     Data(image: "assets/imgs/lasanha.jpg", text: "Lasanha Fit"),
-    Data(
-        image: "assets/imgs/brigadeiro.jpg", text: "Brigadeiro de Batata Doce"),
+    Data(image: "assets/imgs/brigadeiro.jpg", text: "Brigadeiro Fit"),
     Data(image: "assets/imgs/churros.jpg", text: "Churros Fit"),
     Data(image: "assets/imgs/crepioca.jpg", text: "Crepioca Fit"),
     Data(image: "assets/imgs/bolo.jpg", text: "Bolo de Chocolate Fit"),
@@ -38,52 +37,61 @@ class _HomeWidgetState extends State<HomeWidget> {
               fontFamily: 'Lobster', color: Colors.lightGreen, fontSize: 30),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              flex: 40,
-              child: GridView.builder(
-                  itemCount: _photos.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
-                      onTap: () {
-                        _telasReceitas(index);
-                      },
-                      child: Column(
-                        children: [
-                          Container(
-                            width: double.infinity,
-                            height: 170,
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 3, color: Colors.white),
-                              borderRadius: BorderRadius.circular(30),
-                              image: DecorationImage(
-                                  image: AssetImage(_photos[index].image),
-                                  fit: BoxFit.cover),
+      body: SingleChildScrollView(
+        // Adicione o SingleChildScrollView aqui
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              GridView.builder(
+                shrinkWrap:
+                    true, // Permite que o GridView use apenas o espaço necessário
+                physics:
+                    const NeverScrollableScrollPhysics(), // Desabilita a rolagem do GridView
+                itemCount: _photos.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.90, // Ajuste a proporção
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      _telasReceitas(index);
+                    },
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 150, // Ajuste a altura para evitar overflow
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(30),
+                            image: DecorationImage(
+                              image: AssetImage(_photos[index].image),
+                              fit: BoxFit.cover,
                             ),
                           ),
-                          const SizedBox(
-                            height: 1,
-                          ),
-                          Text(
-                            _photos[index].text,
-                            style: TextStyle(
-                                fontFamily: 'PoppinsM',
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-            ),
-          ],
+                        ),
+                        const SizedBox(
+                            height: 8), // Espaço entre a imagem e o texto
+                        Text(
+                          _photos[index].text,
+                          style: TextStyle(
+                              fontFamily: 'PoppinsM',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                            height: 20), // Espaço extra abaixo do texto
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -91,45 +99,28 @@ class _HomeWidgetState extends State<HomeWidget> {
 
   void _telasReceitas(int index) {
     if (index == 0) {
-      setState(() {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ReceitaCoxinha()));
-      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ReceitaCoxinha()));
     }
-
     if (index == 1) {
-      setState(() {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ReceitaLasanha()));
-      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ReceitaLasanha()));
     }
-
     if (index == 2) {
-      setState(() {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => ReceitaBrigadeiro()));
-      });
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => ReceitaBrigadeiro()));
     }
-
     if (index == 3) {
-      setState(() {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ReceitaChurros()));
-      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ReceitaChurros()));
     }
-
     if (index == 4) {
-      setState(() {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => ReceitaCrepioca()));
-      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ReceitaCrepioca()));
     }
-
     if (index == 5) {
-      setState(() {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => ReceitaBolo()));
-      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ReceitaBolo()));
     }
   }
 }
